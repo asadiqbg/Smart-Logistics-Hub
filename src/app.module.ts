853ba27cea29ac9aa5c197625062f071f23898c0
asadiqbg <asadiqbg2000@gmail.com>
2025-10-25 18:35:38 +0500

    Injecting config module and typeormModule in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigFactory } from './config/typeorm.config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV.trim()}`,
+      load: [databaseConfig, appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfigFactory,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
