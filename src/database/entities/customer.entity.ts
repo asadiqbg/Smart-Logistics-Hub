cd44762625e564fa1c5c82c43e8df51078585045
asadiqbg <asadiqbg2000@gmail.com>
2025-10-29 20:15:44 +0500

    Customer Entity

---
+++ src/database/entities/customer.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+  Index,
+} from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Tenant } from './tenant.entity';
+
+@Entity('customers')
+export class Customer extends BaseEntity {
+  @Column({ name: 'tenant_id' })
+  @Index()
+  tenantId: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column()
+  phone: string;
+}
