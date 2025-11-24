6ae6b7d52051a968f5f75d9f1b128cfb1c3610d7
asadiqbg <asadiqbg2000@gmail.com>
2025-11-24 21:03:15 +0500

    feat(orders):OrderControllers routes setup

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CurrentTenant } from 'src/common/decorators/tenant.decorator';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { TenantGuard } from 'src/auth/guards/tenant.guard';
+import { OrderQueryDto } from './dto/order-query.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
 
+@UseGuards(JwtAuthGuard, TenantGuard)
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(
+    @CurrentTenant() tenantId: string,
+    @CurrentUser('id') userId: string,
+    @Body() createOrderDto: CreateOrderDto,
+  ) {
+    return this.ordersService.create(tenantId, userId, createOrderDto);
+  }
+
+  @Get()
+  findAll(@CurrentTenant() tenantId: string, @Query() query: OrderQueryDto) {
+    return this.ordersService.findAll(tenantId, query);
+  }
+
+  @Get(':id')
+  findOne(
+    @CurrentTenant() tenantId: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return this.ordersService.findOne(tenantId, id);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @CurrentTenant() tenantId: string,
+    @CurrentUser('id') userId: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateStatusDto: UpdateOrderStatusDto,
+  ) {
+    return this.ordersService.updateStatus(
+      tenantId,
+      id,
+      userId,
+      updateStatusDto,
+    );
+  }
+}
