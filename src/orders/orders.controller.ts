8121f620b7cce891f58bdcc83df3f2acc27f79d5
asadiqbg <asadiqbg2000@gmail.com>
2025-12-01 18:27:57 +0500

    Docs(orders): Adding swagger api endpoints

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseUUIDPipe,
   Patch,
@@ -17,12 +19,22 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { TenantGuard } from 'src/auth/guards/tenant.guard';
 import { OrderQueryDto } from './dto/order-query.dto';
 import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('orders')
+@ApiBearerAuth()
 @UseGuards(JwtAuthGuard, TenantGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @ApiOperation({ summary: 'Create a new order' })
+  @ApiResponse({ status: 201, description: 'Order created successfully' })
   @Post()
   create(
     @CurrentTenant() tenantId: string,
@@ -32,11 +44,15 @@ export class OrdersController {
     return this.ordersService.create(tenantId, userId, createOrderDto);
   }
 
+  @ApiOperation({ summary: 'Get all orders' })
+  @ApiResponse({ status: 200, description: 'Orders retreived successfully' })
   @Get()
   findAll(@CurrentTenant() tenantId: string, @Query() query: OrderQueryDto) {
     return this.ordersService.findAll(tenantId, query);
   }
 
+  @ApiOperation({ summary: 'Get order by Id' })
+  @ApiResponse({ status: 200, description: 'Order retreived successfully' })
   @Get(':id')
   findOne(
     @CurrentTenant() tenantId: string,
@@ -46,6 +62,9 @@ export class OrdersController {
   }
 
   @Patch(':id/status')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Update order status' })
+  @ApiResponse({ status: 200, description: 'Status updated successfully' })
   updateStatus(
     @CurrentTenant() tenantId: string,
     @CurrentUser('id') userId: string,
